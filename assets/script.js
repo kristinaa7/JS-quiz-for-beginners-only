a99d65e8b8473986e2f8a9a19fcab4f10263ea4d
Kristina Alabastro <kristinaa7@live.com>
2022-12-04 19:38:50 -0800

    added functions startquiz and rendernext

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,98 @@
 //creating timer
 var timeEl = document.querySelector(".time");
-var secondsLeft = 60;
+var secondsLeft = 75;
+var timerInverval ;
 
-var questionEl = document.querySelector(".questions");
+var container = document.querySelector(".container");
+var start = document.querySelector(".start");
+var questionsTitle = document.getElementById("questionsTitle");
+var choice1 = document.querySelector(".choice1");
+var choice2 = document.querySelector(".choice2");
+var choice3 = document.querySelector(".choice3");
+var choice4 = document.querySelector(".choice4");
+var startbutton = document.getElementById('start-button');
+var questionEl = document.getElementById("questions");
+
+startbutton.addEventListener ("click", startquiz);
+choice1.addEventListener ("click", renderNext);
+choice2.addEventListener ("click", renderNext);
+choice3.addEventListener ("click", renderNext);
+choice4.addEventListener ("click", renderNext);
+
 var questions = [
-    {"question": "Commonly used data types DO Not Include:", "options": ["1. strings", "2. booleans", "3. alerts", "4. numbers"], "answer": "4. numbers"},
-    {"question": "The condition is an if / else statement is enclosed with _______.", "options": ["1: quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"], "answer": "2. curly brackets"},
-    {"question": "Arrays in Javascript can be used to store.", "options": ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"], "answer": "4. all of the above"},
-    {"question": "String values must be enclosed within ______ when being assigned to variables.", "options": ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"], "answer": "3. quotes"},
-    {"question": "A very useful tool used during development and debugging for printing content to the debugger is:", "options": ["1. Javascript", "2. terminal/bash", "3. for loops", "4. console.log"], "answer": "4. console.log"},
+    {"question": "Commonly used data types DO Not Include:", 
+    "options": ["1. strings", "2. booleans", "3. alerts", "4. numbers"], 
+    "answer": "4. numbers"},
+    {"question": "The condition is an if / else statement is enclosed with _______.", 
+    "options": ["1: quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"], 
+    "answer": "2. curly brackets"},
+    {"question": "Arrays in Javascript can be used to store.", 
+    "options": ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"], 
+    "answer": "4. all of the above"},
+    {"question": "String values must be enclosed within ______ when being assigned to variables.", 
+    "options": ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"], 
+    "answer": "3. quotes"},
+    {"question": "A very useful tool used during development and debugging for printing content to the debugger is:", 
+    "options": ["1. Javascript", "2. terminal/bash", "3. for loops", "4. console.log"], 
+    "answer": "4. console.log"},
 ];
 
-console.log(questions);
+
+// var score = 0;
+// for (let index = 0; index < questions.length; index++) {
+//     const questionEl = questions[index];
+
+// // if (questionEl == questions[index])
+// //     score++;
+// //     alert("Correct!");
+
+// }
+
+
+// alert("you got " + score + "/")
+// console.log(questions);
 
 //making the timer work
 //get the timer to start when start quiz button is clicked
 function countdown() {
-    var timerInverval = setInterval(function() {
+     timerInverval = setInterval(function() {
         secondsLeft--;
-        timeEl.textContent = "Time: " + secondsLeft + ".";
+        timeEl.textContent = "Time: " + secondsLeft;
 //if question is wrong 15 seconds is deleted
         if(secondsLeft === 0) {
             clearInterval(timerInverval)
-            sendMessage()
+            // sendMessage()
         }
     }, 1000); 
 }
+var questionindex = 0
 
-function sendMessage() {
-    timeEl.textContent = "Congrats, you have completed the quiz.";
-  }
+
+function startquiz() {
+    countdown()
+    container.classList.add("hide");
+    questionEl.classList.remove("hide");
+//calling the function to show the array 
+    questionsTitle.textContent = questions[questionindex].question
+    choice1.textContent = questions[questionindex].options[0]
+    choice2.textContent = questions[questionindex].options[1]
+    choice3.textContent = questions[questionindex].options[2]
+    choice4.textContent = questions[questionindex].options[3]
+}
+
+function renderNext(event) {
+     console.log(event.target.textContent) 
+     console.log(event)
+    questionindex++
+    questionsTitle.textContent = questions[questionindex].question
+    choice1.textContent = questions[questionindex].options[0]
+    choice2.textContent = questions[questionindex].options[1]
+    choice3.textContent = questions[questionindex].options[2]
+    choice4.textContent = questions[questionindex].options[3]
+
+}
+
+// function sendMessage() {
+//     timeEl.textContent = "Congrats, you have completed the quiz.";
+//   }
 
