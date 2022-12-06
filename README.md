ef6ea7be573493a8d35476f307c0103e590c8e79
Kristina Alabastro <kristinaa7@live.com>
2022-12-05 23:45:30 -0800

    adjusted README title and added new functions in script

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# JavaScript Quiz for Beginners Only Please
+# Beginner Coding Multiple Choice Quiz
 
 ## Description 
 
-We have all been there before. Javascript was once new for us all. This quiz was designed specificially for those new to the game of coding with limited to no understanding of Javascript. Here you will have 5 questions that test your beginner Javascript knowledge to help you get started with understanding the basics. You will have 75 seconds to complete the test, but beware as the time will decrement if an incorrect answer is chosen. 
+We have all been there before. Coding was once new for us all. This quiz was designed specificially for those new to the game of coding with limited to no understanding of coding. Here you will have 5 questions that test your beginner coding knowledge to help you get started with understanding the basics. You will have 75 seconds to complete the test, but beware as the time will decrement if an incorrect answer is chosen. 
 
 ## Licenses
 
@@ -12,10 +12,10 @@ N/A
 
 To begin the quiz, first read the instructions. The start button is located in the center of the page and will begin the timer of 75 seconds. The first multiple choice question will pop up with 4 options. Beware as an incorrect answer will deduct your time by 15 seconds. Choosing the correct answer will add an extra second to your timer. This continues for all five questions. Once you reach the last and final question, each of the options will move you to the end screen that presents your score and enables you to add your initials that will then be saved to local storage for later access. When you have reached the end, you can circle back and start the quiz from the beginning or view your highscores.
 
-### Preview Picture
-![alt text](assets/firstpage.png)
+### Screenshots of the Website
+![alt text](images/firstpage.png)
 
-### Link To Webpage
+### Link To Live Webpage
 
 ## Credits
 N/A
--- assets/script.js
+++ assets/script.js
@@ -11,22 +11,26 @@ var choice3 = document.querySelector(".choice3");
 var choice4 = document.querySelector(".choice4");
 var startbutton = document.getElementById('start-button');
 var questionEl = document.getElementById("questions");
-var goBack = document.getElementById("goBack");
-var clearScores = document.getElementById("clearScores");
+var goBack = document.querySelector("goBack-button");
+var clearScores = document.querySelector("clearScores-button");
 var response = document.getElementById("response")
 var endScreen = document.getElementById("end-screen")
 var highscores = document.querySelectorAll(".highscores")
 
-//allows us to keep track of the question you are on the quiz
+//Keeps track of the question and score you are on
 var questionindex = 0
 var score = 0
 
+//Clickable buttons that cause a specific function to run
 startbutton.addEventListener ("click", startquiz);
 choice1.addEventListener ("click", renderNext);
 choice2.addEventListener ("click", renderNext);
 choice3.addEventListener ("click", renderNext);
 choice4.addEventListener ("click", renderNext);
+goBack.addEventListener ("click", resetGame);
+highscores.addEventListener ("click", seehighscores);
 
+//Array of questions, possible answer choices, correct answer
 var questions = [
     {"question": "Commonly used data types DO Not Include:", 
     "options": ["1. strings", "2. booleans", "3. alerts", "4. numbers"], 
@@ -45,19 +49,19 @@ var questions = [
     "answer": "4. console.log"},
 ];
 
-//making the timer work
-//get the timer to start when start quiz button is clicked
+//The countdown function gets the timer to start when startquiz button is clicked
 function countdown() {
      timerInverval = setInterval(function() {
         secondsLeft--;
         timeEl.textContent = secondsLeft;
-//if question is wrong 15 seconds is deleted
+//When time runs out, clears internal
         if(secondsLeft === 0) {
             clearInterval(timerInverval)
         }
     }, 1000); 
 }
 
+//The startquiz function shows each question and has the countdown function to begin
 function startquiz() {
     countdown()
     container.classList.add("hide");
@@ -83,8 +87,8 @@ function renderNext(event) {
         console.log('Incorrect');
         answerFeedback("Incorrect");
         secondsLeft = secondsLeft-15
-        }
-
+        }        
+    }
 
     questionindex++
     questionsTitle.textContent = questions[questionindex].question
@@ -92,7 +96,7 @@ function renderNext(event) {
     choice2.textContent = questions[questionindex].options[1]
     choice3.textContent = questions[questionindex].options[2]
     choice4.textContent = questions[questionindex].options[3]
-}
+
 
 //causes the feedback to show
 function answerFeedback(Correct) {
@@ -104,3 +108,10 @@ function finalpage() {
     endScreen.classList.remove("hide");
 }
 
+function resetGame() {
+
+}
+
+function seehighscores() {
+
+}
